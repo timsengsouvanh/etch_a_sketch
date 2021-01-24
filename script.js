d4cbef84cf2d087649409e01bd73ebaf08bff72c
Tim Sengsouvanh <timsengsouvanh@gmail.com>
2021-01-24 22:05:29 +0900

    added another button

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const container = document.querySelector('#container');
 const body = document.querySelector('body');
+const buttonarea = document.createElement('div');
 
+buttonarea.classList.add('buttonarea')
+body.appendChild(buttonarea)
 
 //create grid squares where i=amount of squares
-for (let i = 0; i<256; i++){
+for (let i = 0; i<1024; i++){
     const grid = document.createElement('div');
     grid.classList.add('blue');
     container.appendChild(grid);
@@ -14,17 +17,29 @@ document.querySelectorAll(".blue").forEach(blue =>
     blue.addEventListener("mouseover", () => blue.classList.add("yellow"))
 );
 
-function createbutton(){
+function createbutton1(){
     const button = document.createElement('button')
     button.textContent = 'CLEAR'
     button.classList.add('button');
-    body.appendChild(button);
+    buttonarea.appendChild(button);
 }
-createbutton()
+createbutton1()
+
+function createbutton2(){
+    const button = document.createElement('button')
+    button.textContent = 'RESIZE'
+    button.classList.add('button');
+    buttonarea.appendChild(button);
+}
+
+createbutton2()
 
 const button = document.querySelector('button');
 
+
+//event listener to clear drawing
 button.addEventListener('click', () => {
     document.querySelectorAll('.yellow').forEach(yellow => yellow.classList.remove('yellow'))
 })
-   
+
+
