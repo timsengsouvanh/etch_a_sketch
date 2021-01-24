59418729978dc3b54781f06cbf6a5ef91454f217
Tim Sengsouvanh <timsengsouvanh@gmail.com>
2021-01-24 18:52:36 +0900

    added hover class change via js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 const container = document.querySelector('#container');
 
-
-//create grid
-// function creategrid(){
-//     grid.classList.add('blue')
-//     container.appendChild(grid);
-// }
-
 for (let i = 0; i<256; i++){
     const grid = document.createElement('div');
     grid.classList.add('blue');
     container.appendChild(grid);
 }
+
+document.querySelectorAll(".blue").forEach(blue => 
+    blue.addEventListener("mouseover", () => blue.classList.add("yellow"))
+);
