a355d1df0875ea07d41143238df12b24bd6b2e58
Tim Sengsouvanh <timsengsouvanh@gmail.com>
2021-01-28 17:49:55 +0900

    changed from flexwrap to making individual flex boxes

--- script.js
+++ script.js
@@ -5,17 +5,51 @@ const buttonarea = document.createElement('div');
 buttonarea.classList.add('buttonarea')
 body.appendChild(buttonarea)
 
+// //create grid squares where i=amount of squares
+// function creategrid(a) {for (let i = 0; i<a; i++){
+//     let grid = document.createElement('div');
+//     grid.classList.add('blue');
+//     container.appendChild(grid);
+// }}
+// creategrid(1024)
+
 //create grid squares where i=amount of squares
-for (let i = 0; i<1024; i++){
-    const grid = document.createElement('div');
-    grid.classList.add('blue');
-    container.appendChild(grid);
-}
+function creategridflex(a) {for (let i = 0; i<a; i++){
+    let gridflex = document.createElement('div');
+    gridflex.classList.add('gridflex');
+    container.appendChild(gridflex);
+}}
+
+creategridflex(30)
+
+function creategrid(a) {for (let i=0;i<a;i++){
+    // let grid = document.createElement('div');
+    // grid.classList.add('blue');
+    // document.querySelectorAll('.gridflex').forEach(gridflex => gridflex.appendChild(grid))
+    let gridflex = document.querySelectorAll('.gridflex')
+    gridflex.forEach(function(gridflex) {
+        let grid = document.createElement('div');
+        grid.classList.add('blue');
+        gridflex.appendChild(grid);
+    })
+    // for (i=0; i<gridflex.length;i++){
+    //     let grid = document.createElement('div');
+    //     grid.classList.add('blue');  
+    //     gridflex[i].appendChild(grid);
+        
+    // }
+}} 
+
+
+creategrid(30)
+
 
 //changes color of squares by adding .yellow class on mouseover
-document.querySelectorAll(".blue").forEach(blue => 
+function changecolor () {document.querySelectorAll(".blue").forEach(blue => 
     blue.addEventListener("mouseover", () => blue.classList.add("yellow"))
-);
+)};
+
+changecolor()
 
 function createbutton1(){
     const button = document.createElement('button')
@@ -45,7 +79,12 @@ button1.addEventListener('click', () => {
 //event listener to resize/clear
 button2.addEventListener('click', () => {
     document.querySelectorAll('.yellow').forEach(yellow => yellow.classList.remove('yellow'))
+    document.querySelectorAll('.blue').forEach(blue => blue.remove())
     let userinput = prompt('How many rows? (max 100)', 100)
-
+    creategridflex(userinput)
+    creategrid(userinput)
+    changecolor()
     }
 )
+
+
